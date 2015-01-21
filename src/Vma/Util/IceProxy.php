95371c087ed2dc1d6d1d4dc05454e9693f845186
ThisIsAreku <alexandre.boucey@gmail.com>
2015-01-21 11:08:13 +0100

    Updated IceProxy

--- src/Vma/Util/IceProxy.php
+++ src/Vma/Util/IceProxy.php
@@ -6,15 +6,33 @@ class IceProxy
 {
     private $iceHost;
     private $iceSecret;
+    private $meta;
 
-    function __construct($iceHost, $iceSecret)
+    function __construct($iceHost, $iceSecret, $sliceIncludeFile, $iceIncludePath = null)
     {
         if (!extension_loaded('ice')) {
             die('Ice is missing');
         }
 
+        if (!empty($iceIncludePath)) {
+            $separator = ':';
+            if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+                $separator = ';';
+            }
+            set_include_path(get_include_path().$separator.$iceIncludePath);
+        }
+
+        if (!file_exists($sliceIncludeFile)) {
+            die('Slice file is missing');
+        }
+
+        require_once 'Ice.php';
+        require_once $sliceIncludeFile;
+
         $this->iceHost = $iceHost;
         $this->iceSecret = $iceSecret;
+
+        $this->loadIce();
     }
 
     private function loadIce()
@@ -24,16 +42,41 @@ class IceProxy
         } else {
             $this->loadIce34();
         }
-    }
 
-    private function loadIce34()
-    {
+        echo 'end';
 
+        die;
     }
 
     private function loadIce33()
     {
+        echo __METHOD__;
+        global $ICE;
+        Ice_loadProfile();
 
+        try {
+            $conn = $ICE->stringToProxy($this->iceHost);
+            $this->meta = $conn->ice_checkedCast('::Murmur::Meta');
+            var_dump($this->meta);
+        } catch (\Ice_ProxyParseException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    private function loadIce34()
+    {
+        $initData = new \Ice_InitializationData();
+        $initData->properties = Ice_createProperties();
+        $initData->properties->setProperty('Ice.ImplicitContext', 'Shared');
+        $ICE = Ice_initialize($initData);
+
+        try {
+            $this->meta = \Murmur_MetaPrxHelper::checkedCast($ICE->stringToProxy($this->iceHost));
+            var_dump($this->meta);
+            $this->meta = $this->meta->ice_context(['secret' => $this->iceSecret]);
+        } catch (\Ice_ConnectionRefusedException $e) {
+            echo "Cannot connect to backend";
+        }
     }
 
 
