d322137913addb5e4d96bf11b5324fe3b6aad61d
ThisIsAreku <alexandre.boucey@gmail.com>
2015-02-06 18:50:51 +0100

    Routes and controller

---
+++ src/Vma/Bundle/HubBundle/Controller/ControlController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexandre
+ * Date: 06/02/15
+ * Time: 18:35
+ */
+
+namespace Vma\Bundle\HubBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Vma\Domain\Murmur\Model\MurmurServer;
+
+class ControlController extends Controller
+{
+    public function startstopAction($serverId)
+    {
+        $murmurMeta = $this->get('murmur.meta');
+
+//        try {
+            /** @var MurmurServer $server */
+            $server = $murmurMeta->getServer($serverId);
+            if ($server->isRunning()) {
+                $server->stop();
+            } else {
+                $server->start();
+            }
+//
+//        } catch (\Exception $e) {
+//            throw new NotFoundHttpException();
+//        }
+
+        return $this->redirect($this->generateUrl('hub.edit', ['serverId' => $serverId]));
+    }
+
+}
--- src/Vma/Bundle/HubBundle/Controller/DefaultController.php
+++ src/Vma/Bundle/HubBundle/Controller/DefaultController.php
@@ -3,11 +3,41 @@
 namespace Vma\Bundle\HubBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\CssSelector\Exception\InternalErrorException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('HubBundle:Default:index.html.twig', array('name' => 'r'));
+        $murmurMeta = $this->get('murmur.meta');
+
+        try {
+            $allServers = $murmurMeta->getAllServers();
+        } catch (\Exception $e) {
+            throw new InternalErrorException();
+        }
+
+        return $this->render('HubBundle:Default:index.html.twig', ['allServers' => $allServers]);
+    }
+
+    public function editAction($serverId)
+    {
+        $murmurMeta = $this->get('murmur.meta');
+
+        try {
+            $server = $murmurMeta->getServer($serverId);
+        } catch (\Exception $e) {
+            throw new NotFoundHttpException();
+        }
+
+        return $this->render('HubBundle:Default:edit.html.twig', ['serverId' => $serverId, 'server' => $server]);
+    }
+
+    public function confAction()
+    {
+        $conf = $this->get('murmur.meta')->getDefaultConf();
+
+        return $this->render('HubBundle:Default:conf.html.twig', ['conf' => $conf]);
     }
 }
