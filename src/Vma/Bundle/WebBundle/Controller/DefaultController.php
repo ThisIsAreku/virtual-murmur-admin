b1a556aefa9093a923e362e1914cb4be10b9b210
ThisIsAreku <alexandre.boucey@gmail.com>
2015-06-24 09:04:38 +0200

    Fixed exception namespace

--- src/Vma/Bundle/WebBundle/Controller/DefaultController.php
+++ src/Vma/Bundle/WebBundle/Controller/DefaultController.php
@@ -3,18 +3,18 @@
 namespace Vma\Bundle\WebBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         $murmurMeta = $this->get('murmur.meta');
-
-        try {
-            $aliveServers = $murmurMeta->getBootedServers();
-        } catch (\Exception $e) {
-            throw new InternalErrorException();
+        if ($murmurMeta == null) {
+            throw new \Exception();
         }
+
+        $aliveServers = $murmurMeta->getBootedServers();
 
         return $this->render('WebBundle::index.html.twig', ['aliveServers' => $aliveServers]);
     }
@@ -22,6 +22,9 @@ class DefaultController extends Controller
     public function viewAction($serverId)
     {
         $murmurMeta = $this->get('murmur.meta');
+        if ($murmurMeta == null) {
+            throw new \Exception();
+        }
 
         try {
             $server = $murmurMeta->getServer($serverId);
